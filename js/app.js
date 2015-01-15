e828626aefb878d6bde6c329f08c0365ebdc98ee
Yusuf Iqbal <ysf.iqbl@gmail.com>
2015-01-15 20:30:54 +0530

    Added player and enemy collision detection.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(n, x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.name = n;
     this.x = x;
     this.y = y;
+    this.width = 60
+    this.height = 60;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -18,7 +21,8 @@ Enemy.prototype.update = function(dt) {
     if (this.x > ctx.canvas.width) {
         this.x = -100;
     }
-    this.x = this.x + 100*dt;
+   this.x = this.x + 100*dt;
+   this.checkCollisions();
 }
 
 // Draw the enemy on the screen, required method for game
@@ -26,13 +30,25 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+Enemy.prototype.checkCollisions = function() {
+    if (this.x < player.x + player.width && this.x + this.width > player.x
+    && this.y < player.y + player.height && this.y + this.width > player.y) {
+        player.reset();
+        console.log(this.name + ": ("+Math.round(this.x) + "," + this.y + ") (" + player.x + "," + player.y + ")");
+    }
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-boy.png';
-    this.x = 200;
-    this.y = 380;
+    this.origin = {x: 200, y: 390}
+    this.x = this.origin.x;
+    this.y = this.origin.y;
+    this.width = 60;
+    this.height= 70;
+    this.lives = 3;
 }
 
 Player.prototype.update = function(dt) {
@@ -70,14 +86,29 @@ Player.prototype.handleInput = function(keyCode) {
         default:
         break;
     }
-    console.log(this.y);
 }
+
+Player.prototype.checkCollisions = function() {
+
+};
+
+Player.prototype.reset = function() {
+    this.lives--;
+    if (this.lives == 0) {
+        alert("Game Over");
+    } else {
+        alert(this.lives + " remaining.")
+    }
+    console.log(this.lives);
+    this.x = this.origin.x;
+    this.y = this.origin.y;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var e1 = new Enemy(1,60);
-var e2 = new Enemy(400,145);
-var e3 = new Enemy(200,230);
+var e1 = new Enemy("top",1,60);
+var e2 = new Enemy("mid",400,145);
+var e3 = new Enemy("bot", 200,230);
 
 var allEnemies = [e1, e2, e3];
 //allEnemies.push();
